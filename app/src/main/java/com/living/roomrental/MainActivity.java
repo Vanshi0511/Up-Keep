a499f300b50c7996440dfd5a1f2e3c3302eb80eb
Sanskar <sanskarpatidar14@gmail.com>
2023-03-21 16:01:00 +0530

    initial commit

---
+++ app/src/main/java/com/living/roomrental/MainActivity.java
@@ -0,0 +1,14 @@
+package com.living.roomrental;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
